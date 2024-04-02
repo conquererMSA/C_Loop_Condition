e7b860b5d89db16feef71f62852a9aa9871d217c
asus test <msa-99@outlook.com>
2024-04-02 18:51:38 +0600

    problem 2

--- operator.c
+++ operator.c
@@ -1,6 +1,22 @@
 //arithmetic operator return value:+,_,*,| and modulas %
 //relational operator return true or false:<,>,==,!=,<=,>=
-#include<stdio.h>
-int main(){
-    
-}
+//logical operator duita ralational operator er moddhyekhane bose condition er upor bose true false return kore
+#include <stdio.h>
+    int main()
+    { 
+        //Practice Day 1: Problem 2
+        long long int num1, num2;
+        scanf("%lld %lld",&num1,&num2);
+        //adding
+        long long int add=(num1+num2);
+        printf("%lld + %lld = %lld \n",num1, num2, add);
+        //substraction 
+        long long int sub=num1-num2;
+        printf("%lld - %lld = %lld \n",num1,num2,sub);
+        //multiplication
+        long long int multi=num1*num2;
+        printf("%lld * %lld = %lld \n",num1,num2,multi);
+        //divide
+         float divide=num2/(num1*1.0);
+        printf("%lld / %lld = %0.1llf",num1,num2,divide);
+    }
